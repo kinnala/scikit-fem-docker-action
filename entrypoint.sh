760358ff7f514250b78cce95f3d06c4d3ac9b4b3
Tom Gustafsson <tom.gustafsson@iki.fi>
2020-10-06 11:53:51 +0300

    Add action.yml

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,9 @@
-#!/bin/sh -l
+#!/bin/bash
 
-git clone https://github.com/kinnala/scikit-fem.git
-cd scikit-fem && pytest -k test_examples
+if [[ -z "${GITHUB_WORKSPACE}" ]]; then
+    git clone https://github.com/kinnala/scikit-fem.git
+    TEST_DIR="scikit-fem"
+else
+    TEST_DIR="${GITHUB_WORKSPACE}"
+fi
+cd $TEST_DIR && pytest -k test_examples
